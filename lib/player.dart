7f483e15b7b7c4363578519fc4aa2f402895d232
Harishramachandran24 <harish.ramachandran24@gmail.com>
2022-02-16 17:58:57 +0530

    Initial commit

---
+++ lib/player.dart
@@ -0,0 +1,244 @@
+import 'dart:ui';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class player extends StatefulWidget {
+  final List playList;
+  const player(this.playList, {Key? key}) : super(key: key);
+
+  @override
+  _playerState createState() => _playerState();
+}
+
+class _playerState extends State<player> with TickerProviderStateMixin {
+  late AnimationController _animationIconController;
+  late AudioCache audioCache;
+  late AudioPlayer audioPlayer;
+  Duration duration = Duration();
+  late int running_seconds;
+  late int playing_seconds;
+  Duration position = Duration();
+  bool issongplaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if(mounted){
+      initPlayer();
+    }
+      setState(() {
+        issongplaying=true;
+        _animationIconController.forward();
+        audioCache.play(widget.playList[0]["song"]);
+      });
+  }
+
+  void initPlayer() {
+    if(mounted){
+      _animationIconController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 450),
+        reverseDuration: const Duration(milliseconds: 450),
+      );
+      audioPlayer = AudioPlayer();
+      audioCache = AudioCache(fixedPlayer: audioPlayer);
+      audioPlayer.onDurationChanged.listen((d) {
+        setState(() {
+          running_seconds=d.inSeconds;
+          duration = d;
+        });
+      });
+      audioPlayer.onAudioPositionChanged.listen((p) {
+        setState(() {
+          position = p;
+          playing_seconds=p.inSeconds;
+        });
+        if(running_seconds==playing_seconds){
+          setState(() {
+            _animationIconController.reverse();
+            issongplaying=false;
+          });
+        }
+      });
+    }
+  }
+
+  void seekToSecond(int second) {
+    Duration newDuration = Duration(seconds: second);
+    audioPlayer.seek(newDuration);
+  }
+
+  Widget slider() {
+    return Slider.adaptive(
+        activeColor: Colors.red,
+        inactiveColor: Colors.black45,
+        min: 0.0,
+        value: position.inSeconds.toDouble(),
+        max: duration.inSeconds.toDouble(),
+        onChanged: (double value) {
+            setState(() {
+              seekToSecond(value.toInt());
+              value = value;
+            });
+        });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final double _height = MediaQuery.of(context).size.height;
+    final double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.white, Colors.deepPurple],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  stops: [0.0, 2.0],
+                  tileMode: TileMode.clamp)),
+        ),
+        Positioned(
+          top: _height*0.18,
+            left: _width*0.09,
+            right: _width*0.09,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                Column(
+                  children:  [
+                    Text(widget.playList[0]["name"],style: const TextStyle(color: Colors.white,fontSize: 18.0,fontWeight: FontWeight.bold),),
+                    Text(widget.playList[0]["musicby"],style: const TextStyle(color: Colors.white),)
+                  ],
+                )
+              ],
+            )),
+        Positioned(
+          height: _height / 5,
+          top: _height * 0.50,
+          left: 10,
+          width: _width - 20,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(height: _height*0.05),
+                 slider(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0,left: 28.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(position.toString().split(".")[0]),
+                      Text(duration.toString().split(".")[0]),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(onPressed:(){
+                        if(position.inSeconds.toDouble()<30.0){
+                          setState(() {
+                            seekToSecond(0);
+                          });
+                        }else{
+                          var value=position.inSeconds.toDouble()-30.0;
+                          setState(() {
+                            seekToSecond(value.toInt());
+                          });
+                        }
+
+                      }, icon: Icon(Icons.replay_30)),
+                      GestureDetector(
+                        onTap: () {
+                          print("s");
+                          if(issongplaying){
+                            setState(() {
+                              _animationIconController.reverse();
+                              audioPlayer.pause();
+                              issongplaying=false;
+                            });
+                          }
+                          else{
+                            print("els");
+                            setState(() {
+                              _animationIconController.forward();
+                              audioCache.play(widget.playList[0]["song"]);
+                              issongplaying=true;
+                            });
+                          }
+
+                        },
+                        child: ClipOval(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                color: Colors.black, shape: BoxShape.circle),
+                            child: Center(
+                                child: AnimatedIcon(
+                              icon: AnimatedIcons.play_pause,
+                              progress: _animationIconController,
+                              color: Colors.red,
+                              size: 40,
+                            )),
+                          ),
+                        ),
+                      ),
+                      IconButton(onPressed:(){
+                        var value=position.inSeconds.toDouble()+30.0;
+                        if(value>duration.inSeconds){
+                          setState(() {
+                            seekToSecond(duration.inSeconds);
+                            _animationIconController.reverse();
+                            issongplaying=false;
+                          });
+                        }else{
+                          setState(() {
+                            seekToSecond(value.toInt());
+                          });
+                        }
+
+                      }, icon: const Icon(Icons.forward_30)),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          height: _height * 0.3,
+          top: _height * 0.25,
+          left: (_width - 220) / 2,
+          right: (_width - 220) / 2,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+              image: DecorationImage(
+                  image: NetworkImage(widget.playList[0]["image"]),
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationIconController.dispose();
+    audioPlayer.dispose();
+  }
+}
+
---
+++ lib/select_music.dart
@@ -0,0 +1,254 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:music_demo/player.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _selectedMusic = '';
+  List all_songs = [
+    {
+      "image":
+      "https://www.iwmbuzz.com/wp-content/uploads/2019/08/what-makes-a-r-rahmans-music-so-popular-920x518.jpg",
+      "name": "Hayati",
+      "song": "Hayati.mp3",
+      "musicby": "A R Rahman"
+    },
+    {
+      "image":
+      "https://static.toiimg.com/thumb/msid-73098630,width-1200,height-900,resizemode-4/.jpg",
+      "name": "Arabic Kuthu",
+      "song": "ArabicKuthu.mp3",
+      "musicby": "Anirudh"
+    },
+  ];
+  List playList = [];
+  late AudioCache audioCache;
+  late AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    final double _height = MediaQuery.of(context).size.height;
+    final double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.white, Colors.deepPurple],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.0, 2.0],
+                  tileMode: TileMode.clamp)),
+          child: Stack(
+            children: [
+              Positioned(
+                height: _height * 0.2,
+                top: _height * 0.14,
+                left: (_width - 170) / 2,
+                right: (_width - 170) / 2,
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      image: const DecorationImage(
+                          image: NetworkImage(
+                              "https://media.istockphoto.com/photos/golden-music-notes-isolated-on-white-background-picture-id1182235520?b=1&k=20&m=1182235520&s=170667a&w=0&h=HN1r8AhdrQX-amWYM9vpv0WANR0OoVTnbihMg5v4hA8="),
+                          fit: BoxFit.cover),
+                    )),
+              ),
+              Positioned(
+                top: _height / 2,
+                height: _height,
+                left: 0.0,
+                right: 0.0,
+                child: Card(
+                    elevation: 5,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                "Playlist",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: _height * 0.02,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: _height * 0.08,
+                                width: _width - 40,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    border: Border.all(
+                                        color: Colors.black45, width: 2.0)),
+                                child: Row(
+                                  children: [
+                                    Radio<String>(
+                                        value: "arrahman",
+                                        groupValue: _selectedMusic,
+                                        onChanged: (value) {
+                                          playList = [];
+                                          setState(() {
+                                            _selectedMusic = value!;
+                                            if (_selectedMusic == "arrahman") {
+                                              playList.add(all_songs[0]);
+                                              print(playList);
+                                            }
+                                          });
+                                        }),
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                          image: const DecorationImage(
+                                              image: NetworkImage(
+                                                  "https://www.iwmbuzz.com/wp-content/uploads/2019/08/what-makes-a-r-rahmans-music-so-popular-920x518.jpg"),
+                                              fit: BoxFit.cover),
+                                          borderRadius:
+                                          BorderRadius.circular(10.0)),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                        children: const [
+                                          Text("Hayati"),
+                                          Text("A.R.Rahman")
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: _height * 0.02,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: _height * 0.08,
+                                width: _width - 40,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    border: Border.all(
+                                        color: Colors.black45, width: 2.0)),
+                                child: Row(
+                                  children: [
+                                    Radio<String>(
+                                        value: "anirudh",
+                                        groupValue: _selectedMusic,
+                                        onChanged: (value) {
+                                          playList = [];
+                                          setState(() {
+                                            _selectedMusic = value!;
+                                            if (_selectedMusic == "anirudh") {
+                                              playList.add(all_songs[1]);
+                                              print(playList);
+                                            }
+                                          });
+                                        }),
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                          image: const DecorationImage(
+                                              image: NetworkImage(
+                                                  "https://static.toiimg.com/thumb/msid-73098630,width-1200,height-900,resizemode-4/.jpg"),
+                                              fit: BoxFit.cover),
+                                          borderRadius:
+                                          BorderRadius.circular(10.0)),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                        children: const [
+                                          Text("Arabic Kuthu"),
+                                          Text("Anirudh")
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )),
+              ),
+              Positioned(
+                  bottom: _height * 0.05,
+                  left: (_width - 100) / 2,
+                  right: (_width - 100) / 2,
+                  height: 50,
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.red,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "PLAY",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        )
+                      ],
+                    ),
+                    onPressed: () {
+                      if (playList.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text("select a song"),
+                              ],
+                            )));
+                      } else {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => player(playList)),
+                        );
+                      }
+                    },
+                  ))
+            ],
+          ),
+        ));
+  }
+}
